f2379e7b8343cc87e9846e36910008ba9d0aa631
Joan Roca Hormaza <remnyachizot2015@gmail.com>
2023-01-04 12:08:49 -0500

    Verify error api/user route

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -6,39 +6,43 @@ const HandlerUser = async (req: NextApiRequest, res: NextApiResponse) => {
   //Only POST mothod is accepted
   switch (req.method) {
     case 'POST':
-      //Getting email and password from body
-      const { name, lastName, email, password } = req.body;
-      //Validate
-      if (!name || !lastName || !email || !email.includes('@') || !password)
-        return res.status(422).json({ message: 'Invalid Data' });
+      try {
+        //Getting email and password from body
+        const { name, lastName, email, password } = req.body;
+        //Validate
+        if (!name || !lastName || !email || !email.includes('@') || !password)
+          return res.status(422).json({ message: 'Invalid Data' });
 
-      const connection = await connectToDatabase();
-      if (!connection) return;
+        const connection = await connectToDatabase();
+        if (!connection) return;
 
-      const db = connection.db('lerchat');
-      if (db) {
-        const checkExisting = await db
-          .collection('users')
-          .findOne({ email: email });
-        //Send error response if duplicate user is found
-        if (checkExisting) {
-          res.status(422).json({ message: 'User already exists' });
-          // connection.mongoClient.close();
-          return;
+        const db = connection.db('lerchat');
+        if (db) {
+          const checkExisting = await db
+            .collection('users')
+            .findOne({ email: email });
+          //Send error response if duplicate user is found
+          if (checkExisting) {
+            res.status(422).json({ message: 'User already exists' });
+            // connection.mongoClient.close();
+            return;
+          }
+          //Hash password
+          const status = await db.collection('users').insertOne({
+            ...req.body,
+            image: 'default.png',
+            avatar: 'default.png',
+            password: await hash(password, 12)
+          });
+          //Send success response
+          res.status(201).json({ message: 'User created', ...status });
+          //Close DB connection
+          // connection?.mongoClient.close();
+        } else {
+          res.status(500).json({ message: 'Database not Working' });
         }
-        //Hash password
-        const status = await db.collection('users').insertOne({
-          ...req.body,
-          image: 'default.png',
-          avatar: 'default.png',
-          password: await hash(password, 12)
-        });
-        //Send success response
-        res.status(201).json({ message: 'User created', ...status });
-        //Close DB connection
-        // connection?.mongoClient.close();
-      } else {
-        res.status(500).json({ message: 'Database not Working' });
+      } catch (err) {
+        res.status(500).json({ message: 'Server Error', err });
       }
       break;
 
